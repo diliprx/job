a460dfe4768dbdf1f46b558800de3043dddf7e34
diliprx <dilipvenkat2006@gmail.com>
2025-12-11 11:28:29 +0530

    Update drop text color in CTFTeamWebsite.jsx from red-700 to red-600

--- C1CC4D0/frontend/src/pages/CTFTeamWebsite.jsx
+++ C1CC4D0/frontend/src/pages/CTFTeamWebsite.jsx
@@ -171,7 +171,7 @@ const CTFTeamWebsite = () => {
         {drops.map((drop) => (
           <div
             key={drop.id}
-            className="absolute font-mono text-red-700"
+            className="absolute font-mono text-red-600"
             style={{
               left: `${drop.x}px`,
               top: `${drop.y}px`,
